5b8d887c71ab51198d242d921e089a38d3c9029f
no-vr <84609700+no-vr@users.noreply.github.com>
2021-05-25 22:14:02 +0800

    Update hello.ml

--- hello.ml
+++ hello.ml
@@ -6,5 +6,11 @@
 <h4>HELLO</h4>
 <h5>HELLO</h5>
 <h6>HELLO</h6>
+<form oninput="x.value=parseInt(a.value)+parseInt(b.value)">
+<input type="range" id="a" value="100">
++<input type="number" id="b" value="69">
+=<output name="x" for="a b"></output>
+</form>
+
 </body>
 </html>
